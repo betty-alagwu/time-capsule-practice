3b1ba0627ca2cdf9eb69843ac5c35c8c6ab30e1e
Betty Alagwu <bettyalagwu@gmail.com>
2023-03-18 20:22:47 +0100

    chore: section 2 / lesson 3 - store stream from user media into ref

--- pages/recording.js
+++ pages/recording.js
@@ -1,14 +1,18 @@
 import { useEffect } from "react"
+import { useRef } from "react"
 
 
 export default function Recording() {
+ const mediaStreamRef = useRef(null)
 
 useEffect(() => {
  async function getUserPermission() {
    const mediaStream = await navigator.mediaDevices.getUserMedia({video: true, audio: true })
+   mediaStreamRef.current = mediaStream
  }
  getUserPermission()
 }, [])
+
 
  return (
    <h1>
