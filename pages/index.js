7c11ac17e04f39d8a63693661d81da13dec2bab0
Betty Alagwu <bettyalagwu@gmail.com>
2023-03-18 19:05:54 +0100

    chore: section 1 / lesson 3 - add link from home page to recording page

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,18 @@
+import Link from 'next/link'
+
+
 
 export default function Home() {
   return (
-    <h1>
+    <>
+      <h1>
       Home page
     </h1>
+    <Link href={"/recording"}>
+      <button>
+        Record a message
+      </button>
+    </Link>
+    </>
   )
 }
