1d8a9ea9379e6a023e1dcd91863ef28f9b7de22b
Betty Alagwu <bettyalagwu@gmail.com>
2023-03-18 20:54:38 +0100

    chore: section 2 / lesson 4 - store and show error message in state

--- pages/recording.js
+++ pages/recording.js
@@ -1,22 +1,31 @@
 import { useEffect } from "react"
 import { useRef } from "react"
+import { useState } from "react"
 
 
 export default function Recording() {
  const mediaStreamRef = useRef(null)
+ const [errorMessage, setErrorMessage] = useState('')
 
 useEffect(() => {
  async function getUserPermission() {
-   const mediaStream = await navigator.mediaDevices.getUserMedia({video: true, audio: true })
-   mediaStreamRef.current = mediaStream
+   try{
+    const mediaStream = await navigator.mediaDevices.getUserMedia({video: true, audio: true })
+    mediaStreamRef.current = mediaStream
+ }catch(error){
+  setErrorMessage("Please grant permission to use your camera and microphone")
+  }
  }
- getUserPermission()
+  getUserPermission()
 }, [])
 
 
  return (
+  <>
    <h1>
      Recording page
    </h1>
+   {errorMessage && <p>{errorMessage}</p>}
+  </>
  )
 }
