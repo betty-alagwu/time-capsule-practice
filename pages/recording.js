e9822d2c0b1e9fb46a54945a8ee316d99a79f3b7
Betty Alagwu <bettyalagwu@gmail.com>
2023-03-18 20:05:02 +0100

    chore: section 2 / lesson 2 - add navigator user media in the useEffect

--- pages/recording.js
+++ pages/recording.js
@@ -1,5 +1,15 @@
+import { useEffect } from "react"
+
 
 export default function Recording() {
+
+useEffect(() => {
+ async function getUserPermission() {
+   const mediaStream = await navigator.mediaDevices.getUserMedia({video: true, audio: true })
+ }
+ getUserPermission()
+}, [])
+
  return (
    <h1>
      Recording page
